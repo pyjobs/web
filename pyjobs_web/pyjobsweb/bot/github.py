d21e898f9e1dee78cce1a738af5fd8bfcfe74064
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-19 12:09:04 +0100

    Github bot: add comments

--- pyjobs_web/pyjobsweb/bot/github.py
+++ pyjobs_web/pyjobsweb/bot/github.py
@@ -35,17 +35,30 @@ class GitHubBot(object):
         self._check_repository()
 
     def _check_repository_directory(self):
+        """
+        Create repository directory if not exist
+        :return:
+        """
         if not os.path.exists(self._repository_path):
             os.makedirs(self._repository_path)
 
     def _check_repository(self):
+        """
+        Check if repository exist and clone it if not.
+        Then fill _repo instance attribute et make a pull.
+        :return:
+        """
         if not os.path.exists("%s/.git" % self._repository_path):
             Repo.clone_from(self.REPOSITORY_ADDRESS, self._repository_path)
 
-        self._repo =  Repo(self._repository_path)
+        self._repo = Repo(self._repository_path)
         self._pull()
 
     def run(self):
+        """
+        Update job file if new jobs. Then make a push.
+        :return:
+        """
         lasts_jobs = self._get_lasts_jobs()
         new_jobs = self._get_new_jobs(lasts_jobs)
 
@@ -56,6 +69,10 @@ class GitHubBot(object):
             self._push()
     
     def _get_lasts_jobs(self):
+        """
+        :return: x last jobs, where x is _last_jobs_count class attribute
+        :rtype: list
+        """
         lasts_jobs = model.DBSession \
             .query(model.data.Job) \
             .order_by(model.data.Job.publication_datetime.desc()) \
@@ -64,6 +81,10 @@ class GitHubBot(object):
         return lasts_jobs
 
     def _get_new_jobs(self, jobs):
+        """
+        :param jobs: job to parse
+        :return: jobs not listed in jobs file
+        """
         cleaned_jobs = []
         with codecs.open(self._jobs_file_path, 'r', 'utf-8') as jobs_file:
             jobs_file_read = jobs_file.read()
@@ -73,15 +94,30 @@ class GitHubBot(object):
         return cleaned_jobs
 
     def _write_jobs(self, jobs):
+        """
+        Write jobs in jpobs file
+        :param jobs: list of jobs
+        :return:
+        """
         template = Template(filename=self._jobs_template_file_path)
         with codecs.open(self._jobs_file_path, 'w', 'utf-8') as jobs_file:
             print(template.render(jobs=jobs), file=jobs_file)
     
     def _get_commit_message(self, jobs):
+        """
+        :param jobs:
+        :return: Message rended from message template, where template file is specified in
+         _jobs_message_file_path class attribute
+        """
         template = Template(filename=self._jobs_message_file_path)
         return template.render(jobs=jobs)
     
     def _commit(self, message):
+        """
+        Make the commit
+        :param message: commit message
+        :return:
+        """
         repo_index = self._repo.index
         repo_index.add([self._jobs_file_path])
         # TODO - B.S. - 20160118: Le mail est ok ?
@@ -99,6 +135,9 @@ class GitHubBot(object):
             origin.push()
 
     def _get_remote_environnement(self):
+        """
+        :return: Return an environment who specify to use class attribute _deployment_key_file_path key
+        """
         ssh_cmd = 'ssh -i %s' % self._deployment_key_file_path
         return self._repo.git.custom_environment(GIT_SSH_COMMAND=ssh_cmd)
 
