277f5e4edca75e5a60e3a98ef1daa04324792ed7
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-06-23 16:15:17 +0200

    Research form improvements

--- pyjobs_web/pyjobsweb/controllers/root.py
+++ pyjobs_web/pyjobsweb/controllers/root.py
@@ -58,7 +58,7 @@ class RootController(BaseController):
         tmpl_context.project_name = "Algoo"
 
     @staticmethod
-    def _get_specific_job_offers(keywords=None):
+    def _get_specific_job_offers(query=None, from_location=None, max_dist=None):
         fields = ["description", "title"]
 
         res = model.JobOfferElasticsearch.search()\
@@ -78,11 +78,13 @@ class RootController(BaseController):
 
     @expose('pyjobsweb.templates.jobs')
     @paginate('jobs', items_per_page=20)
-    def index(self, keywords=None, geoloc=None):
-        if not keywords:
+    def index(self, query=None, from_location=None, max_dist=None):
+        if not query and not from_location and not max_dist:
             job_offers = self._get_all_job_offers()
         else:
-            job_offers = self._get_specific_job_offers(keywords)
+            job_offers = self._get_specific_job_offers(
+                    query, from_location, max_dist
+            )
 
         search_form = ResearchForm(action='/', method='POST').req()
 
--- pyjobs_web/pyjobsweb/forms/ResearchForm.py
+++ pyjobs_web/pyjobsweb/forms/ResearchForm.py
@@ -4,23 +4,72 @@ import tw2.forms as twf
 
 
 class ResearchForm(twf.Form):
-    class child(twf.TableLayout):
-        keywords = twf.TextField(
-            "keywords", label="", validator=twc.Required
-        )
-        keywords.css_class = "form-control"
-        keywords.placeholder = u"Mot clés recherchés..."
-        keywords.value = ""
-        keywords.name = "keywords"
+    class child(twf.widgets.BaseLayout):
+        inline_engine_name = "mako"
+        template = \
+            u'''
+            <div class="container">
+                <h2>Rechercher un emploi</h2>
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="query">Requête :</label>
+                        <div class="col-sm-10">
+                            ${w.children.query.display()|n}
+                        </div>
+                    </div>
+                    <div class="col-xs-12" style="height:3px;"></div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="from_location">Autour de :</label>
+                        <div class="col-sm-10">
+                            ${w.children.from_location.display()|n}
+                        </div>
+                    </div>
+                    <div class="col-xs-12" style="height:3px;"></div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="max_dist">Dans un rayon de :</label>
+                        <div class="col-sm-10">
+                            ${w.children.max_dist.display()|n}
+                        </div>
+                    </div>
+                    <div class="col-xs-12" style="height:3px;"></div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-md-10">
+                            ${w.submit.display()|n}
+                        </div>
+                    </div>
+                </form>
+            </div>
+            '''
 
-        geoloc = twf.TextField(
-            "geoloc", label="", validator=twc.Required
+        query = twf.TextField(
+                name="query", label=""
         )
-        geoloc.css_class = "form-control"
-        geoloc.placeholder = u"Géolocalisation..."
-        geoloc.value = ""
-        geoloc.name = "geoloc"
+        query.css_class = "form-control"
+        query.placeholder = u"Mot clés recherchés..."
+        query.value = ""
 
-    submit = twf.SubmitButton("submit")
-    submit.value = "Go !"
-    submit.css_class = "btn btn-default right"
+        from_location = twf.TextField(
+                name="from_location", label=""
+        )
+        from_location.css_class = "form-control"
+        from_location.placeholder = u"Géolocalisation..."
+        from_location.value = ""
+
+        max_dist = twf.SingleSelectField(
+                name="max_dist", label=""
+        )
+        max_dist.css_class = "form-control col-sm-12"
+        distances = [
+            "5", "10", "25", "50", "100", "200", "200+"
+        ]
+        distances_km = []
+        for d in distances:
+            distances_km.append("{}{}".format(d, "km"))
+        max_dist.options = distances_km
+        max_dist.prompt_text = "Distance maximale"
+
+        submit = twf.SubmitButton("submit")
+        submit.value = "J'effectue ma recherche"
+        submit.css_class = "btn btn-default form-control"
+
+    submit = None
