f917ecb43d2072d737a13609e42dc7be71c08fd6
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-07-26 16:33:20 +0200

    PopulateES indexing both geocomplete and jobs index
    The populateES is now capable of indexing both geocomplete and jobs index
    of the elasticsearch database.

--- pyjobs_web/pyjobsweb/commands/populate_es.py
+++ pyjobs_web/pyjobsweb/commands/populate_es.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import elasticsearch_dsl
-import elasticsearch
 import logging
+import json
+from elasticsearch_dsl.connections import connections
+from elasticsearch.helpers import parallel_bulk
+
+from tg import config
 
 import pyjobsweb.model as model
 import pyjobsweb.lib.geolocation as geolocation
@@ -13,62 +17,131 @@ class PopulateESCommand(AppContextCommand):
         super(PopulateESCommand, self).__init__(args, kwargs)
         self._geolocator = geolocation.Geolocator()
 
+    def get_parser(self, prog_name):
+        parser = super(PopulateESCommand, self).get_parser(prog_name)
+
+        jobs_help_msg = 'populates the jobs index of the elasticsearch database'
+        parser.add_argument('-j', '--jobs',
+                            help=jobs_help_msg,
+                            dest='populate_jobs_index',
+                            action='store_const', const=True)
+
+        geocomplete_help_msg = \
+            'populates the geocomplete index of the elasticsearch database'
+        parser.add_argument('-g', '--geocomplete',
+                            help=geocomplete_help_msg,
+                            dest='populate_geocomplete_index',
+                            action='store_const', const=True)
+
+        return parser
+
     @staticmethod
-    def _error_logging(job_id, message, logging_level):
+    def _jobs_error_logging(job_id, message, logging_level):
         err_msg = u'[Job offer id: %s] %s.' % (job_id, message)
         logging.getLogger(__name__).log(logging_level, err_msg)
 
-    def _handle_insertion_task(self, job_offer):
-        es_job_offer = job_offer.to_elasticsearch_job_offer()
+    def _compute_geoloc(self):
+        # We first try to recompute the geolocation of job offers whose
+        # geolocation could not be computed earlier. (Timeout...).
+        to_geoloc_query = \
+            model.ElasticsearchQuery(model.JobOfferElasticsearch, 0, 10000)
+        import pyjobsweb.lib.search_query as sq
+        to_geoloc_query. \
+            add_elem(sq.BooleanFilter('geolocation_error', True))
+        to_geoloc = to_geoloc_query.execute_query()
 
-        # Compute lat, lng of the job offer
-        try:
-            location = self._geolocator.geocode(job_offer.address)
+        for document in to_geoloc:
+            try:
+                location = self._geolocator.geocode(document.address)
+                document.update(geolocation=dict(lat=location.latitude,
+                                                 lon=location.longitude),
+                                geolocation_error=False)
+            except geolocation.BaseError as e:
+                self._jobs_error_logging(document.id, e, logging.WARNING)
 
-            es_job_offer.geolocation = dict(lat=location.latitude,
-                                            lon=location.longitude)
-            es_job_offer.geolocation_error = False
-        except geolocation.BaseError as e:
+    @staticmethod
+    def _compute_job_offers_elasticsearch_documents():
+        pending_insertion_job_offers = \
+            model.JobOfferSQLAlchemy.compute_elasticsearch_pending_insertions()
+
+        res = list()
+
+        for job_offer in pending_insertion_job_offers:
+            es_job_offer = job_offer.to_elasticsearch_job_offer()
             es_job_offer.geolocation = dict(lat=0.0, lon=0.0)
             es_job_offer.geolocation_error = True
-            self._error_logging(job_offer.id, e, logging.WARNING)
 
-        # Perform the insertion in Elasticsearch
-        try:
-            es_job_offer.save()
-        except elasticsearch.exceptions.RequestError as e:
-            err_msg = u'Elasticsearch insertion error: %s, %s.' % (e, e.info)
-            self._error_logging(job_offer.id, err_msg, logging.ERROR)
+            res.append(es_job_offer)
+
+        return res
+
+    @staticmethod
+    def _geocompletion_documents():
+        geolocation_data = open(config.get('fr.geolocation_data.path'))
+
+        json_dict = json.loads(geolocation_data.read())
+
+        for postal_code, places in json_dict.items():
+            for place in places:
+                yield model.Geocomplete(name=place['name'],
+                                        postal_code=postal_code,
+                                        geolocation=dict(
+                                            lat=float(place['lat']),
+                                            lon=float(place['lon'])
+                                        ),
+                                        weight=place['weight'])
+
+    def populate_jobs_index(self):
+        elasticsearch_conn = connections.get_connection()
+        pending_insertions = self._compute_job_offers_elasticsearch_documents()
+
+        to_index = (d.to_dict(True) for d in pending_insertions)
+
+        bulk_results = enumerate(parallel_bulk(elasticsearch_conn, to_index))
+
+        for i, (ok, info) in bulk_results:
+            job_id = pending_insertions[i].id
+
+            if ok:
+                # Mark the task as handled so we don't retreat it next time
+                model.JobOfferSQLAlchemy. \
+                    mark_as_inserted_in_elasticsearch(job_id)
+            else:
+                doc_id = info['create']['_id']
+                doc_type = info['create']['_type']
+                doc_index = info['create']['_index']
+
+                err_msg = "Couldn't index document: '%s', of type: %s, " \
+                          "under index: %s." % (doc_id, doc_type, doc_index)
+                self._jobs_error_logging(job_id, err_msg, logging.ERROR)
+
+        # Refresh indices to increase research speed
+        elasticsearch_dsl.Index('jobs').refresh()
+
+    def populate_geocomplete_index(self):
+        elasticsearch_conn = connections.get_connection()
+        to_index = (d.to_dict(True) for d in self._geocompletion_documents())
+
+        for ok, info in parallel_bulk(elasticsearch_conn, to_index):
+            if not ok:
+                doc_id = info['create']['_id']
+                doc_type = info['create']['_type']
+                doc_index = info['create']['_index']
+
+                logging_level = logging.ERROR
+                err_msg = "Couldn't index document: '%s', of type: %s, " \
+                          "under index: %s." % (doc_id, doc_type, doc_index)
+
+                logging.getLogger(__name__).log(logging_level, err_msg)
+
+        elasticsearch_dsl.Index('geocomplete').refresh()
 
     def take_action(self, parsed_args):
         super(PopulateESCommand, self).take_action(parsed_args)
 
-        # We first try to recompute the geolocation of job offers whose
-        # geolocation could not be computed earlier. (Timeout...).
-        failed_geolocs_query = \
-            model.ElasticsearchQuery(model.JobOfferElasticsearch, 0, 10000)
-        import pyjobsweb.lib.search_query as sq
-        failed_geolocs_query. \
-            add_elem(sq.BooleanFilter('geolocation_error', True))
-        failed_geolocs = failed_geolocs_query.execute_query()
+        if parsed_args.populate_geocomplete_index:
+            self.populate_geocomplete_index()
 
-        for f in failed_geolocs:
-            try:
-                location = self._geolocator.geocode(f.address)
-                f.update(location=dict(lat=location.latitude,
-                                       lon=location.longitude),
-                         geolocation_error=False)
-            except geolocation.BaseError as e:
-                self._error_logging(f.id, e, logging.WARNING)
-
-        job_offers = model.JobOfferSQLAlchemy. \
-            compute_elasticsearch_pending_insertions()
-
-        for j in job_offers:
-            self._handle_insertion_task(j)
-            # Mark the task as handled so we don't retreat it next time
-            model.JobOfferSQLAlchemy. \
-                mark_as_inserted_in_elasticsearch(j.id)
-
-        # Refresh indices to increase research speed
-        elasticsearch_dsl.Index('jobs').refresh()
+        if parsed_args.populate_jobs_index:
+            self.populate_jobs_index()
+            self._compute_geoloc()
