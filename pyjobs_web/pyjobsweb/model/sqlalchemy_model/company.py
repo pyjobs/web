81c3bce71382f34487cd35a35ed42f868cee7cf0
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-16 09:31:33 +0200

    Company model: technologies field change
    This field doesn't store a JSON file anymore, but rather a simple text field,
    containing a coma separated list of technologies that the company uses.

--- pyjobs_web/pyjobsweb/model/sqlalchemy_model/company.py
+++ pyjobs_web/pyjobsweb/model/sqlalchemy_model/company.py
@@ -13,7 +13,11 @@ class Company(DeclarativeBase):
     logo_url = sa.Column(sa.String(1024), nullable=False, default='')
     description = sa.Column(sa.Text(), nullable=False, default='')
     url = sa.Column(sa.String(1024), nullable=False, default='')
-    technologies = sa.Column(sa.Text(), nullable=False, default='')  # JSON
+    # Coma separated technologies. We only store a textual representation of
+    # the technologies used by the company to make it easier to duplicate the
+    # model into Elasticsearch to allow searching on this field (Elasticsearch
+    # is really good at string research).
+    technologies = sa.Column(sa.Text(), nullable=False, default='')
     address = sa.Column(sa.String(1024), nullable=False, default='')
     email = sa.Column(sa.String(1024), nullable=False, default='')
     phone = sa.Column(sa.String(1024), nullable=False, default='')
