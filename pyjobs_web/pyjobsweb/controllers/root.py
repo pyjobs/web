ed56ad67397be5064be74d6d60e8375c299f1c32
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-17 09:27:24 +0200

    PEP8

--- pyjobs_web/pyjobsweb/controllers/root.py
+++ pyjobs_web/pyjobsweb/controllers/root.py
@@ -19,7 +19,7 @@ from pyjobsweb.controllers.error import ErrorController
 from pyjobsweb.controllers.geocomplete import GeocompleteController
 from pyjobsweb.controllers.search import SearchController
 from pyjobsweb.controllers.secure import SecureController
-from pyjobsweb.forms.ResearchForm import ResearchForm
+from pyjobsweb.forms.research_form import ResearchForm
 from pyjobsweb.lib.base import BaseController
 from pyjobsweb.lib.helpers import slugify, get_job_url
 from pyjobsweb.lib.stats import StatsQuestioner
