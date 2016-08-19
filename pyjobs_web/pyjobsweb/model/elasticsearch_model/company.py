ffefa5eda3ab72096e318223a390f642a1b9b151
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-19 11:13:22 +0200

    Type mismatch between the two models

--- pyjobs_web/pyjobsweb/model/elasticsearch_model/company.py
+++ pyjobs_web/pyjobsweb/model/elasticsearch_model/company.py
@@ -58,7 +58,7 @@ class Company(es.DocType):
     technologies_analyzer = es.analyzer('technologies_analyzer',
                                         tokenizer=technologies_tokenizer)
 
-    siren = es.Integer()
+    siren = es.String(index='no')
 
     name = es.String(index='not_analyzed')
     logo_url = es.String(index='no')
