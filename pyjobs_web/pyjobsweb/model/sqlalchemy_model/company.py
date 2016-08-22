992be4521d1fe739b93999554b9b36687642daf9
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-22 11:33:05 +0200

    Typo

--- pyjobs_web/pyjobsweb/model/sqlalchemy_model/company.py
+++ pyjobs_web/pyjobsweb/model/sqlalchemy_model/company.py
@@ -16,7 +16,7 @@ class Company(DeclarativeBase):
     logo_url = sa.Column(sa.String(1024), nullable=False, default='')
     description = sa.Column(sa.Text(), nullable=False, default='')
     url = sa.Column(sa.String(1024), nullable=False, default='')
-    # Coma separated technologies. We only store a textual representation of
+    # Comma separated technologies. We only store a textual representation of
     # the technologies used by the company to make it easier to duplicate the
     # model into Elasticsearch to allow searching on this field (Elasticsearch
     # is really good at string research).
