c1eb1251f2277857942d8c596d1c86321f4a3360
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-23 16:51:28 +0200

    Admin interface customization
    Deep customization of the admin interface. Some routes aren't fully
    implement yet. More to come in the near future.

--- pyjobs_web/pyjobsweb/controllers/admin.py
+++ pyjobs_web/pyjobsweb/controllers/admin.py
@@ -3,14 +3,15 @@ from sprox.widgets import TextField
 from tg.decorators import expose, with_trailing_slash
 from tgext.admin import CrudRestControllerConfig
 from tgext.admin.controller import AdminController
-from tgext.admin.tgadminconfig import BootstrapTGAdminConfig as TGAdminConfig
+from tgext.admin.tgadminconfig import TGAdminConfig, BootstrapTGAdminConfig
+from tgext.admin.layouts import BootstrapAdminLayout
 from tgext.crud import EasyCrudRestController
 
 from pyjobsweb import model
 from pyjobsweb.model import DBSession
 
 
-class InvalidAddressesController(EasyCrudRestController):
+class JobGeocodingController(EasyCrudRestController):
     __table_options__ = {
         '__limit_fields__': ['url', 'id', 'source', 'address',
                              'address_is_valid'],
@@ -23,7 +24,7 @@ class InvalidAddressesController(EasyCrudRestController):
                                    '</span>'
                                    '</a>' % dict(url=row.url),
         '__actions__': lambda filler, row:
-            InvalidAddressesController.__actions__(filler, row)
+            JobGeocodingController.__actions__(filler, row)
     }
 
     __form_options__ = {
@@ -38,7 +39,7 @@ class InvalidAddressesController(EasyCrudRestController):
     }
 
     def __init__(self, session, menu_items=None):
-        super(InvalidAddressesController, self).__init__(session, menu_items)
+        super(JobGeocodingController, self).__init__(session, menu_items)
 
     @classmethod
     def __actions__(cls, filler, row):
@@ -65,7 +66,7 @@ class InvalidAddressesController(EasyCrudRestController):
         # Since this controller is only meant to fix invalid addresses, we only
         # request job offers with invalid addresses.
         kw['address_is_valid'] = False
-        return super(InvalidAddressesController, self).get_all(*args, **kw)
+        return super(JobGeocodingController, self).get_all(*args, **kw)
 
     @expose(inherit=True)
     def post(self, *args, **kw):
@@ -88,38 +89,118 @@ class InvalidAddressesController(EasyCrudRestController):
         return EasyCrudRestController.put(self, *args, **kw)
 
 
-class InvalidAddressesAdminConfig(TGAdminConfig):
+class CompanyGeocodingController(EasyCrudRestController):
+    def __init__(self, session, menu_items=None):
+        super(CompanyGeocodingController, self).__init__(session,
+                                                         menu_items)
+
+    # TODO: implement
+
+
+class CompanyModerationController(EasyCrudRestController):
+    def __init__(self, session, menu_items=None):
+        super(CompanyModerationController, self).__init__(session,
+                                                          menu_items)
+
+    # TODO: implement
+
+
+class GeocodingAdminConfig(BootstrapTGAdminConfig):
+    """
+    PyJobs' geocoding related issues admin controller configuration. Every
+    tables in the model that's prone to having geocoding issues which require
+    a custom CrustRestController to solve, should be configured in this class.
+    See Turbogears 2's documentation for more details:
+    http://turbogears.readthedocs.io/en/latest/turbogears/wikier/admin.html?highlight=crud
+    """
     def __init__(self, models, translations=None):
-        super(InvalidAddressesAdminConfig, self).__init__(models, translations)
+        super(GeocodingAdminConfig, self).__init__(models, translations)
 
     class job(CrudRestControllerConfig):
-        defaultCrudRestController = InvalidAddressesController
+        defaultCrudRestController = JobGeocodingController
+
+    class company(CrudRestControllerConfig):
+        defaultCrudRestController = CompanyGeocodingController
 
 
-class AdminAddressesController(AdminController):
+class ModerationAdminConfig(BootstrapTGAdminConfig):
+    """
+    PyJobs' moderation related issues admin controller configuration. Every
+    tables in the model that's prone to moderation which require a custom
+    CrustRestController should be configured in this class. See Turbogears 2's
+    documentation for more details:
+    http://turbogears.readthedocs.io/en/latest/turbogears/wikier/admin.html?highlight=crud
+    """
+    def __init__(self, models, translations=None):
+        super(ModerationAdminConfig, self).__init__(models, translations)
+
+    class company(CrudRestControllerConfig):
+        defaultCrudRestController = CompanyModerationController
+
+
+class AdminGeocodingController(AdminController):
+    """
+    PyJobs' admin sub-controller in charge of managing geolocation issues
+    related routing in the admin. Every table in the model that might have
+    geocoding issues should be added to the 'to_fix' list.
+    """
+
+    """
+    A list containing every tables in the Postgresql database that might have
+    geocoding issues that should be handled by this controller.
+    """
+    to_fix = [model.JobAlchemy, model.CompanyAlchemy]
+
     def __init__(self):
-        super(AdminAddressesController, self).__init__(
-            [model.JobAlchemy],
-            DBSession,
-            config_type=InvalidAddressesAdminConfig
-        )
+        super(AdminGeocodingController, self).__init__(
+            self.to_fix, DBSession, config_type=GeocodingAdminConfig)
 
 
-class JobsController(EasyCrudRestController):
-    __table_options__ = {
-        '__omit_fields__': [],
-        '__field_order__': ['url', 'id', 'source', 'address',
-                            'address_is_valid'],
-        '__xml_fields__': ['url'],
-        'url': lambda filler, row: '<a class="btn btn-default" '
-                                   'target="_blank" href="%(url)s">'
-                                   '<span class="glyphicon glyphicon-link">'
-                                   '</span>'
-                                   '</a>' % dict(url=row.url)
+class AdminModerationController(AdminController):
+    """
+    PyJobs' admin sub-controller in charge of managing moderation related
+    routing in the admin. Every table in the model that requires moderation
+    should be added to the 'to_moderate' list.
+    """
+
+    """
+    A list containing every tables in the Postgresql database that should be
+    handled by this controller.
+    """
+    to_moderate = [model.CompanyAlchemy]
+
+    def __init__(self):
+        super(AdminModerationController, self).__init__(
+            self.to_moderate, DBSession, config_type=ModerationAdminConfig)
+
+
+class PyJobsAdminLayout(BootstrapAdminLayout):
+    """
+    Pyjobs' custom admin interface layout. Redefines template_index and
+    crud_templates from the ones used in the BootstrapAdminLayout.
+    """
+    template_index = 'pyjobsweb.templates.admin.index'
+    crud_templates = {
+        'get_all': [
+            'mako:pyjobsweb.templates.admin.get_all'
+        ],
+        'edit': [
+            'mako:pyjobsweb.templates.admin.edit'
+        ],
+        'new': [
+            'mako:pyjobsweb.templates.admin.new'
+        ]
     }
 
+
+class JobCrudRestController(EasyCrudRestController):
+    """
+    Not a whole lot of changes in this custom EasyCrudRestController. It just
+    adds some database synchronization code (to keep both Postgresql and
+    Elasticsearch databases in sync.
+    """
     def __init__(self, session, menu_items=None):
-        super(JobsController, self).__init__(session, menu_items)
+        super(JobCrudRestController, self).__init__(session, menu_items)
 
     @expose(inherit=True)
     def put(self, *args, **kw):
@@ -136,24 +217,109 @@ class JobsController(EasyCrudRestController):
         return EasyCrudRestController.put(self, *args, **kw)
 
 
+class CompanyCrudRestController(EasyCrudRestController):
+    """
+    Not a whole lot of changes in this custom EasyCrudRestController. It just
+    adds some database synchronization code (to keep both Postgresql and
+    Elasticsearch databases in sync.
+    """
+    def __init__(self, session, menu_items=None):
+        super(CompanyCrudRestController, self).__init__(session, menu_items)
+
+    @expose(inherit=True)
+    def put(self, *args, **kw):
+        # The row has been modified, therefore this row is now dirty, and
+        # should be resynchronized with Elasticsearch. Also, the geolocation
+        # should be recomputed too, so we mark the address as valid, so that
+        # the geolocation program will try and recompute it later on.
+        kw['dirty'] = True
+        return EasyCrudRestController.put(self, *args, **kw)
+
+
 class PyJobsAdminConfig(TGAdminConfig):
-    def __init__(self, models, translations=None):
-        super(PyJobsAdminConfig, self).__init__(models, translations)
+    """
+    The configuration of the PyJobs admin interface. As you can see, this config
+    uses a custom AdminLayout (which redefines some templates used by the
+    admin - like the index for instance).
+    We also tell Turbogears admin to use our custom CrudRestControllers when
+    it comes to editing the jobs and the companies tables
+    """
+    layout = PyJobsAdminLayout
 
     class job(CrudRestControllerConfig):
-        defaultCrudRestController = JobsController
+        defaultCrudRestController = JobCrudRestController
+
+    class company(CrudRestControllerConfig):
+        defaultCrudRestController = CompanyCrudRestController
 
 
 class PyJobsAdminController(AdminController):
-    def __init__(self):
-        super(PyJobsAdminController, self).__init__(model, DBSession,
-                                                    config_type=TGAdminConfig)
+    """
+    This controller handles the whole admin interface of PyJobs
+    It consists of multiple CrudRestControllers that allows one to manipulate
+    the database.
+    The of this controller index points to the default CrudRest
+    AdminController, with a few modifications to ensure some synchronization
+    issues between the Postgresql database and the Elasticsearch database,
+    (through the 'dirty' flag in the Postgresql tables which are to be
+    synchronized with their counterparts in Elasticsearch).
+    """
 
-        self.addresses = AdminAddressesController()
+    """
+    This sub-controller is used to solve any geocoding related issues that
+    might have occured during the geocoding operation performed by the
+    gearbox geocode command.
+    """
+    geocoding = AdminGeocodingController()
+
+    """
+    This controller is used to moderate (edit, delete, accept) company
+    submissions that have been submitted, but haven't yet been moderated.
+    """
+    moderation = AdminModerationController()
+
+    # The clunky 's' after the table names are enforced by the way the
+    # framework generates the admin controllers. So, even though the plural
+    # form in english is a tad more complicated than just appending a 's' at
+    # the end of the words, this is how it is in TG2 and this explains why there
+    # are these 's' at the end of the link parameters.
+    # TODO: find a way to remove the clunky 's'
+    geolocation_list = [
+        {'link': 'geocoding/jobs', 'display': 'Jobs'},
+        {'link': 'geocoding/companys', 'display': 'Companies'}
+    ]
+
+    moderation_list = [
+        {'link': 'moderation/companys', 'display': 'Companies'}
+    ]
+
+    def __init__(self):
+        super(PyJobsAdminController, self).__init__(
+            model, DBSession, config_type=PyJobsAdminConfig)
+
+    @property
+    def model_list(self):
+        models = [table.__name__ for table in self.config.models.values()]
+        models.sort()
+
+        model_list = list()
+        for m in models:
+            model_list.append(dict(link='%ss' % m.lower(), display=m))
+
+        return model_list
 
     @with_trailing_slash
     @expose()
     def index(self):
-        res = super(PyJobsAdminController, self).index()
-        res['models'].append('Addresse')
+        # We use a custom index template, therefore, we have to change the dict
+        # that's returned by the super.index() method, so that our template gets
+        # the values it needs to operate correctly.
+        super_res = super(PyJobsAdminController, self).index()
+
+        res = dict(config=super_res['config'],
+                   model_config=super_res['model_config'],
+                   model_list=self.model_list,
+                   geolocation_list=self.geolocation_list,
+                   moderation_list=self.moderation_list)
+
         return res
