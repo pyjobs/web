7c807e03ade024ac9ad6229b60e3270970a03ec3
Benoit ECHERNIER <benoit.echernier@algoo.fr>
2016-08-30 15:16:16 +0200

    Pyjobs bots cron scripts

---
+++ pyjobs_web/pyjobs_bots.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# CRON Frequency recommendation * 1 * * * *
+# Usage: this_script.sh /path/to/pyjobs /var/log/pyjobs /path/to/production.ini
+
+SOURCE_DIR="$1"
+LOG_DIR="$2"
+CONFIG="$3"
+
+echo SOURCE_DIR: ${SOURCE_DIR}, LOG_DIR: ${LOG_DIR}, CONFIG: ${CONFIG}
+
+cd ${SOURCE_DIR} && . ./venv2.7/bin/activate
+# Run the Github bot
+gearbox -c "$CONFIG" bots github >> "$LOG_DIR"/github_bot.log
+echo ALGOO-MONITORABLE-CRON __ $(date +\"%Y-%m-%dT%H:%M:%S\") __  >> "$LOG_DIR"/elasticsearch_sync_jobs.log
+# Run the Twitter bot - Maximum 250 Tweets
+gearbox -c "$CONFIG" bots twitter -n 250 -c twitter_credentials.json  >> "$LOG_DIR"/twitter_bot.log
+echo ALGOO-MONITORABLE-CRON __ $(date +\"%Y-%m-%dT%H:%M:%S\") __  >> "$LOG_DIR"/elasticsearch_sync_companies.log
